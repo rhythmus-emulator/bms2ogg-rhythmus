453ca42b66239ea74dfe308e75cefa8c9e209c83
kuna <kgdgwn98@gmail.com>
2019-07-02 21:15:54 +0900

    Progress bar for CLI.

--- encoder/main.cpp
+++ encoder/main.cpp
@@ -135,7 +135,16 @@ class REncoder_CLI : public REncoder
 public:
   virtual void OnUpdateProgress(double progress)
   {
-    // TODO
+    constexpr int barWidth = 70;
+    int pos = barWidth * progress;
+    for (int i = 0; i < barWidth; ++i)
+    {
+      if (i < pos) std::cout << "=";
+      else if (i == pos) std::cout << ">";
+      else std::cout << " ";
+    }
+    std::cout << "] " << int(progress * 100.0) << " %\r";
+    std::cout.flush();
   }
 };
 
